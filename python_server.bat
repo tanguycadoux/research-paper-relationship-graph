58435a985adaa4c1ca0bee601c0d77ee450d9d90
tanguycadoux <tanguycadoux04@gmail.com>
2025-04-13 11:32:11 +0200

    test commit

--- python_server.bat
+++ python_server.bat
@@ -2,4 +2,5 @@
 cd /d "E:\Projets webapp\Research papers link\python_server.bat"
 echo Starting Python HTTP server on port 8000...
 python -m http.server 8000
+echo Python HTTP server running on port 8000
 pause
